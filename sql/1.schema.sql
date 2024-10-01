f0d4017e050d066648c07cccdbf96d9259a243bf
whsanha <whsanha55@gmail.com>
2024-10-01 22:15:04 +0900

    docker-compose, db 스키마 & data 추가

--- sql/1.schema.sql
+++ sql/1.schema.sql
@@ -1,5 +1,38 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL,
-                                     password VARCHAR(255) NOT NULL
-);
+drop table if exists  channel;
+drop table if exists  subscription;
+drop table if exists  subscription_history;
+
+CREATE TABLE channel
+(
+    id                BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '채널 고유 ID',
+    channel_name      VARCHAR(100) NOT NULL COMMENT '채널 이름 (플랫폼, 사이트명)',
+    able_subscribe TINYINT not null comment '구독 가능 여부',
+    able_unsubscribe TINYINT not null comment '구독 해지 가능 여부',
+    created_at        DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    updated_at        DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간'
+) COMMENT '구독/해지 가능한 외부 서비스 채널을 관리하는 테이블';
+
+
+CREATE TABLE subscription
+(
+    id                  INT AUTO_INCREMENT PRIMARY KEY COMMENT '구독 상태 고유 ID',
+    phone_number        VARCHAR(256) NOT NULL COMMENT '회원 고유 ID(휴대폰 번호 암호화)',
+    channel_id          INT          NOT NULL COMMENT '채널 고유 ID',
+    subscription_status VARCHAR(50)  NOT NULL COMMENT '회원 구독 상태 (none: 구독 안함, normal: 일반 구독, premium: 프리미엄 구독)',
+    created_at          DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    updated_at          DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+    index (phone_number)
+) COMMENT '회원의 구독 상태 관리 테이블';
+
+CREATE TABLE subscription_history
+(
+    id                      INT AUTO_INCREMENT PRIMARY KEY COMMENT '구독 이력 고유 ID',
+    phone_number         VARCHAR(256)         NOT NULL COMMENT '회원 고유 ID(휴대폰 번호 암호화)',
+    channel_id              INT         NOT NULL COMMENT '채널 고유 ID',
+    old_subscription_status VARCHAR(50) NOT NULL COMMENT '이전 구독 상태 (none, normal, premium)',
+    subscription_status     VARCHAR(50) NOT NULL COMMENT '변경된 구독 상태 (none, normal, premium)',
+    created_at              DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    updated_at              DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+    index (phone_number),
+    index (channel_id)
+) COMMENT '구독 상태 변경 이력 관리 테이블';
--- sql/2.data.sql
+++ sql/2.data.sql
@@ -1,1 +1,22 @@
-INSERT INTO users (username, password) VALUES ('admin', 'admin_password');
+INSERT INTO channel (channel_name, able_subscribe, able_unsubscribe)
+VALUES ('뉴스 채널', 1, 1),
+       ('음악 채널', 1, 0),
+       ('영화 채널', 1, 1),
+       ('스포츠 채널', 1, 1),
+       ('어린이 채널', 1, 0),
+       ('프리미엄 스포츠 채널', 0, 1),
+       ('기술 채널', 1, 1),
+       ('다큐멘터리 채널', 1, 0),
+       ('게임 채널', 1, 1),
+       ('교육 채널', 1, 1);
+
+INSERT INTO subscription (phone_number, channel_id, subscription_status)
+VALUES ('MQ==', 1, 'PREMIUM'),
+       ('MQ==', 2, 'REGULAR'),
+       ('MQ==', 3, 'NONE');
+
+
+INSERT INTO subscription_history (phone_number, channel_id, old_subscription_status, subscription_status)
+VALUES ('MQ==', 1, 'NONE', 'PREMIUM' ),
+       ('MQ==', 2, 'NONE', 'PREMIUM' ),
+       ('MQ==', 3, 'NONE', 'PREMIUM' );
